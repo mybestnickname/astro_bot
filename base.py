ba0d37a54f3a9f5e710ed07e11567e67c8df8d86
mybestnickname <innotimeagh@Gmail.com>
2018-07-24 18:55:17 +0300

    add sqlite

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///:db:', echo=False)
+engine = create_engine('sqlite:///db.sqlite', echo=False)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
