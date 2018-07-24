9ad3c2212bc588ae232e8d816eec14bba4a1c68b
mybestnickname <innotimeagh@Gmail.com>
2018-07-24 09:58:51 +0300

    add quiz hndlrs

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 import logging
 from settings import constells_dict
@@ -41,8 +41,6 @@ def planet_handler(bot, update):
         bot_text = 'Список доступных планет:\n{}'.format(all_pl)
         bot.sendMessage(chat_id=update.message.chat.id, text=bot_text)
         return True
-    # не очень ясно из задания, нужно перебрать КАЖДУЮ планету ifами??
-    # это же не pythonic way :)
     splitted_msg = update.message.text.split(' ')
     # если длина списка больше двух, то меняем дату на указанную
     if len(splitted_msg) > 2:
@@ -98,13 +96,15 @@ def moon_handler(bot, update):
     full_moon_date = ephem.next_full_moon(ephem_date)
     moon_info = ephem.Moon(ephem_date)
     # создадим из них строку
-    bot_text = ("""Луна растёт с {}
+    bot_text = ("""
+        Луна растёт с {}
         Полнолуние наступит: {}
         Луну видно на: {:.3f} %
-        Дистанция до земли: {:.3f} км.""".format(growth_start_date,
-                                                 full_moon_date,
-                                                 moon_info.moon_phase * 100,
-                                                 moon_info.earth_distance * 149600000))
+        Дистанция до земли: {:.3f} км.
+        """.format(growth_start_date,
+                   full_moon_date,
+                   moon_info.moon_phase * 100,
+                   moon_info.earth_distance * 149600000))
     # напишем её в чатик
     bot.sendMessage(chat_id=update.message.chat.id, text=bot_text)
 
@@ -119,8 +119,10 @@ def solar_system_handler(bot, update):
     solar_system = ['Солнце', 'Меркурий', 'Венера', 'Земля',
                     'Марс', 'Юпитер', 'Сатурн', 'Уран', 'Нептун',
                     'Плутон', 'Хаумеа', 'Макемаке', 'Эрида']
-    url_buttons = [InlineKeyboardButton(text=item, url="https://ru.wikipedia.org/wiki/{}".format(item))
-                   for item in solar_system]
+    url_buttons = [
+        InlineKeyboardButton(
+            text=item, url="https://ru.wikipedia.org/wiki/{}".format(item))
+        for item in solar_system]
     custom_keyboard = [url_buttons[:5], url_buttons[5:10], url_buttons[10:]]
     reply_markup = InlineKeyboardMarkup(custom_keyboard)
     bot.send_message(chat_id=update.message.chat.id,
@@ -149,12 +151,15 @@ def sun_handler(bot, update):
     next_ris = moscow_obs.next_rising(sun_info)
     next_set = moscow_obs.next_setting(sun_info)
     earth_dist_km = sun_info.earth_distance * 149600000
-    bot_text = """  В Москве({} ш.):
+    bot_text = """
+    В Москве({} ш.):
     Предыдущий восход: {}
     Предыдущий закат: {}
     Следующий восход: {}
     Следующий закат: {}
-    Дистанция до земли: {:.3f} км.""".format(moscow_obs.lat, previous_ris, previous_set, next_ris, next_set, earth_dist_km)
+    Дистанция до земли: {:.3f} км.
+    """.format(moscow_obs.lat, previous_ris, previous_set,
+               next_ris, next_set, earth_dist_km)
     bot.sendMessage(chat_id=update.message.chat.id, text=bot_text)
 
 
@@ -162,7 +167,7 @@ def message_handler(bot, update):
     """
     Обработчик прямого текста к боту
     """
-    #user_text = update.message.text
+    # user_text = update.message.text
     # logging.info(user_text)
     username = update.message.from_user.username
     update.message.reply_text('@{} используй /help!'.format(username))
@@ -173,7 +178,10 @@ def strange_command_handler(bot, update):
     Функция обрабатывающая странные команды
     Выводит подсказку, что можно ввести /help
     """
-    bot_message = 'Не известная команда.\nИспользуйте /help для отображения списка команд.'
+    bot_message = """
+    Не известная команда.
+    Используйте /help для отображения списка команд.
+    """
     update.message.reply_text(bot_message)
 
 
@@ -185,21 +193,66 @@ def constellations_translator(const_name):
     return constells_dict.get(const_name, const_name)
 
 
-def main():
-    # updt = Updater(TELEGRAM_API_KEY, request_kwargs=PROXY)
-    updt = Updater(TELEGRAM_API_KEY)
-    updt.start_polling()
-    # лучше отлавливать команды декораторами, наверное.
+def quiz_handler(bot, update):
+    """
+    Обработчик команды quiz
+    """
+    # подгружаем 10 случайных вопросов из бд
+    # даём 30 сек на каждый ответ
+    # отсчёт должен производится в отдельном треде
+    # чтоб не блокировать приём ответов
+    bot_text = """
+        Сыграем в викторину?
+        10 вопросов о космосе.
+        30 сек на каждый ответ.
+                """
+    bot.sendMessage(chat_id=update.message.chat.id, text=bot_text)
+    buttons = [InlineKeyboardButton(text='Поехали!',
+                                    callback_data="quiz_offer start"),
+               InlineKeyboardButton(text='Отмена',
+                                    callback_data="quiz_offer cancel"),
+               InlineKeyboardButton(text='Отмена',
+                                    callback_data="quiz_respond Меркурий!")]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    bot.send_message(update.message.chat.id, reply_markup=reply_markup)
+
+
+def quiz_offer_handler(bot, update):
+    """
+    Функция обработки ответов на предложение сыграть в викторину
+    """
+    bot.send_message(update.message.chat.id, text='blblblblbl QUIZ!')
+
+
+def handler_adder(updt):
+    """
+    Функция определяющая функции - обработчики комманд
+    """
     updt.dispatcher.add_handler(CommandHandler("start", start_handler))
     updt.dispatcher.add_handler(CommandHandler("planet", planet_handler))
     updt.dispatcher.add_handler(CommandHandler("moon", moon_handler))
     updt.dispatcher.add_handler(CommandHandler("sun", sun_handler))
     updt.dispatcher.add_handler(CommandHandler("solar", solar_system_handler))
     updt.dispatcher.add_handler(CommandHandler("help", help_handler))
+    updt.dispatcher.add_handler(CommandHandler("quiz", quiz_handler))
     updt.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+    # обработчик ответов от пользователя
+    # на предложение сыграть
+    updt.dispatcher.add_handler(CallbackQueryHandler(quiz_offer_handler,
+                                                     pattern='^quiz_offer.*'))
+    # ответы на вопросы викторины
+    # updt.dispatcher.add_handler(CallbackQueryHandler(quiz_respond_handler,
+    #                                                 pattern='^quiz_respond.*'))
     # обработчик неизвестных комманд в самый конец
     updt.dispatcher.add_handler(MessageHandler(
         Filters.command, strange_command_handler))
+
+
+def main():
+    # updt = Updater(TELEGRAM_API_KEY, request_kwargs=PROXY)
+    updt = Updater(TELEGRAM_API_KEY)
+    updt.start_polling()
+    handler_adder(updt)
     updt.idle()
 
 
