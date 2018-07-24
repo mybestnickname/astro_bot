7a667c8591a634a0a0d151082b4a1292faff8cc7
mybestnickname <innotimeagh@Gmail.com>
2018-07-24 10:12:00 +0300

    add quiz hndlrs

--- bot.py
+++ bot.py
@@ -207,14 +207,14 @@ def quiz_handler(bot, update):
     30 сек на каждый ответ.
     """
     bot.sendMessage(chat_id=update.message.chat.id, text=bot_text)
-    buttons = [InlineKeyboardButton(text='Поехали!',
-                                    callback_data="quiz_offer start"),
-               InlineKeyboardButton(text='Отмена',
-                                    callback_data="quiz_offer cancel"),
-               InlineKeyboardButton(text='Отмена',
-                                    callback_data="quiz_respond Меркурий!")]
-    reply_markup = InlineKeyboardMarkup([buttons])
-    bot.send_message(update.message.chat.id, reply_markup=reply_markup)
+    buttons = [[InlineKeyboardButton(text='Поехали!',
+                                     callback_data="quiz_offer start")],
+               [InlineKeyboardButton(text='Отмена',
+                                     callback_data="quiz_offer cancel")],
+               [InlineKeyboardButton(text='Отмена',
+                                     callback_data="quiz_respond Меркурий!")]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    bot.send_message(chat_id=update.message.chat.id, reply_markup=reply_markup)
 
 
 def quiz_offer_handler(bot, update):
