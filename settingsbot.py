dc3610295705b5a51eeae1d1ec243d64ed8cc834
mybestnickname <innotimeagh@Gmail.com>
2018-08-07 15:33:04 +0300

    last commit, i hope

--- settingsbot.py
+++
@@ -1,9 +0,0 @@
-PROXY = {'proxy_url': 'socks5://prem1.tgresistance.com:3306',
-         'urllib3_proxy_kwargs': {
-             'username': 'user_70645578',
-             'password': 'ov7ecZLCviq47127'}
-         }
-# PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-#         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
-TELEGRAM_API_KEY = "561255866:AAG9eDPHU5FLHlafsjR_ApUzu4SEXX0mmIU"
